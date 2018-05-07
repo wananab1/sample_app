160b64c8638e12b4920190de8fefef5671e5016a
wananab1 <wtnbshut.mij@gmail.com>
2018-05-07 11:47:56 +0000

    Start 3-8

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+ 	def new
+  		#空のモデルをviewに渡す。
+  		@post = Post.new
+  	end
+	def create
+		post = Post.new(post_params)
+		post.save
+		redirect_to post_path(post.id)
+	end
+
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		post = Post.find(params[:id])
+		post.update(post_params)
+		redirect_to post_path(post.id)
+	end
+
+
+ 	private
+
+  	def post_params
+  		params.require(:post).permit(:title, :body)
+ 	 end
+end
