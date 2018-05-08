ef9e6c362303ce95f13ecb5a6a0aee647e0ee69a
Shota Watanabe <wtnbshut.mij@gmail.com>
2018-05-08 14:47:50 +0900

    Finish 3-9

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/posts/:id' => 'posts#show',as: 'post'
   get 'posts/:id/edit' => 'posts#edit' ,as: 'edit_post'
   patch '/posts/:id' => 'posts#update' ,as: 'update_post'
+  delete '/posts/:id' => 'posts#destroy' ,as: 'destroy_post'
 end
